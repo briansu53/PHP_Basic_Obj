36ca8e7ed418fa6a4b610a2adde2f7bc08f2b2dd
brian <penguin410925@gmail.com>
2019-04-25 20:28:43 +0800

    edit content

--- PHP_Basic_Obj/obj.php
+++ PHP_Basic_Obj/obj.php
@@ -1,3 +1,32 @@
 <?php
+//6-3 定義一個類別
+
+class Entree{
+    public $name;
+    public $ingredients = array();
+
+    public function hasIngredient($ingredient){
+        return in_array($ingredient, $this->ingredients);
+    }
+}
+
+//6-2 建立與使用物件
+
+$soup = new Entree;
+$soup->name = 'Chicken Soup';
+$soup->ingredients = array('chicken', 'water');
+
+$sandwich = new Entree;
+$sandwich ->name = 'Chicken sandwitch';
+$sandwich ->ingredients = array('chicken', 'bread');
+
+foreach(['chicken','lemon', 'water','bread'] as $ing){
+    if($soup->hasIngredient($ing)){
+        print "Soup contains $ing.\n";
+    }
+    if($sandwich->hasIngredient($ing)){
+        print "Sandwich contains $ing.\n";
+    }
+}
 
 ?>
